3467a8b612afb03de30212b91e3ed5e81c377a4e
Blake McKenzie <02blake.mckenzie@gulllakecs.org>
2019-12-02 14:07:10 -0500

    Merged the DigitalRead and Serial with Master

--- sketch_nov06a/sketch_nov06a.ino
+++ sketch_nov06a/sketch_nov06a.ino
@@ -6,17 +6,30 @@
  * This program will turn on an LED for specified amuounts of time, 
  * turn it off, and back on in a While Loop.
  * 
+ * 
+ * /*
+ *DigitalReadSerial
+ *Reads a digital input on pin 2, prints the rtesults to the serial monitor
+ *
+ *This example code is inb the public domain
  */
 
 
 // initialized our led to Pin 13
 int led = 13;
-
+int pushButton = 3;
 
 
 void setup() {
   // Using the Function pinMode to call the variable and provide Output:
   pinMode (led, OUTPUT);
+  
+  //initialize seria; communication at 9600 bits per second:
+  Serial.begin(9600);
+  //make the pushbutton's pin an input:
+  pinMode (pushButton, INPUT);
+
+  
 }
 
 void loop() {
@@ -25,4 +38,10 @@ void loop() {
   delay(1000);
   digitalWrite(led, LOW);
   delay(1000);
+
+  //read the input pin:
+  int buttonState = digitalRead(pushButton);
+  //print out the state of the button:
+  Serial.println(buttonState);
+  delay (1);      //delay in between reads for stability
 }
