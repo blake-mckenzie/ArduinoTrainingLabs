f4a1ac1f2816dc8b4e4971abf4f4b94087210968
Blake McKenzie <02blake.mckenzie@gulllakecs.org>
2019-12-03 14:08:01 -0500

    Initial Potentiometer Program

--- sketch_nov06a/sketch_nov06a.ino
+++ sketch_nov06a/sketch_nov06a.ino
@@ -1,18 +1,18 @@
 /*Programmer: Mr. Lange
- * Date: 1.6.19
- * Program:Blink
- * 
- * 
- * This program will turn on an LED for specified amuounts of time, 
- * turn it off, and back on in a While Loop.
- * 
- * 
- * /*
- *DigitalReadSerial
- *Reads a digital input on pin 2, prints the rtesults to the serial monitor
- *
- *This example code is inb the public domain
- */
+   Date: 1.6.19
+   Program:Blink
+
+
+   This program will turn on an LED for specified amuounts of time,
+   turn it off, and back on in a While Loop.
+
+
+   /
+  DigitalReadSerial
+  Reads a digital input on pin 2, prints the rtesults to the serial monitor
+
+  This example code is inb the public domain
+*/
 
 
 // initialized our led to Pin 13
@@ -23,13 +23,13 @@ int pushButton = 3;
 void setup() {
   // Using the Function pinMode to call the variable and provide Output:
   pinMode (led, OUTPUT);
-  
+
   //initialize seria; communication at 9600 bits per second:
   Serial.begin(9600);
   //make the pushbutton's pin an input:
   pinMode (pushButton, INPUT);
 
-  
+
 }
 
 void loop() {
@@ -37,13 +37,14 @@ void loop() {
   int buttonState = digitalRead(pushButton);
   //print out the state of the button:
   Serial.println(buttonState);
-  if (buttonState ==HIGH){
-  digitalWrite(led, HIGH);
+  if (buttonState == HIGH) {
+    digitalWrite(led, HIGH);
   }
-    else {
-      digitalWrite(led, LOW); 
-    }
+  else {
+    digitalWrite(led, LOW);
+  }
 
   {
-  delay (1);      //delay in between reads for stability
-}}
+    delay (1);      //delay in between reads for stability
+  }
+}
